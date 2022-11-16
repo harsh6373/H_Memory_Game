6073de664d03ea25052ea8d01759371a489bd051
Harsh Jani <harshjani6373@gmail.com>
2022-11-16 15:00:16 +0530

    User interface of main screen is done

--- app/src/main/java/com/harsh02/h_memory_game/MainActivity.kt
+++ app/src/main/java/com/harsh02/h_memory_game/MainActivity.kt
@@ -2,10 +2,24 @@ package com.harsh02.h_memory_game
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvnummoves: TextView
+    private lateinit var tvnumpairs: TextView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        rvBoard = findViewById(R.id.rv_board)
+        tvnummoves = findViewById(R.id.tv_num_moves)
+        tvnumpairs = findViewById(R.id.tv_num_pairs)
     }
 }
